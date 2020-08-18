0c743c3f553b136081e09426cc2523f493b7f95d
salmaBadr27 <beauty_saloma@outlook.com>
2020-08-18 23:14:05 +0200

    push

---
+++ readme.md
@@ -0,0 +1,15 @@
+# Weather Journal App :umbrella:
+
+## Project 2 - Weather Journal App (Front End Developer Udacity Nanodegree)
+
+### Project Objective
+
+This project was about working with Web APIs and asynchronous code, creating a web app that uses OpenWeatherMap API to get weather based on user's location, and dynamically update UI.
+
+### Building
+
+To get the project up and running I followed these steps:
+
+- npm i
+- npm start to start server
+- enter zip code and press generate to see temperature and feelings !
---
+++ website/app.js
@@ -0,0 +1,84 @@
+/* Global Variables */
+const generateBtn = document.getElementById("generate");
+const zip = document.getElementById("zip").value;
+const feelings = document.getElementById("feelings").value;
+
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
+
+// Personal API Key for OpenWeatherMap API
+const baseURL = "http://api.openweathermap.org/data/2.5/weather?zip=";
+const apiKey = ",us&appid=0bc1cb5bd6690e0fa4495ba9bdd96db1";
+
+// Event listener to add function to existing HTML DOM element
+generateBtn.addEventListener("click", performAction);
+
+/* Function called by event listener */
+
+function performAction(e) {
+	e.preventDefault();
+	getWeather(baseURL, zip, apiKey)
+		.then(function (userData) {
+			// add data to POST request
+			postData("/add", {
+				date: newDate,
+				temp: userData.main.temp,
+				content: feelings,
+			});
+		})
+		.then(function (newData) {
+			// call updateUI to update browser content
+			updateUI();
+		});
+}
+
+/* Function to GET Web API Data*/
+const getWeather = async (baseURL, zip, apiKey) => {
+	// res equals to the result of fetch function
+	const res = await fetch(baseURL + zip + apiKey);
+	console.log(zip);
+	try {
+		// userData equals to the result of fetch function
+		const userData = await res.json();
+		return userData;
+	} catch (error) {
+		console.log("error", error);
+	}
+};
+/* Function to POST data */
+const postData = async (url = "", data = {}) => {
+	const req = await fetch(url, {
+		method: "POST",
+		credentials: "same-origin",
+		headers: {
+			"Content-Type": "application/json;charset=UTF-8",
+		},
+		body: JSON.stringify({
+			date: data.date,
+			temp: data.temp,
+			content: data.content,
+		}),
+	});
+
+	try {
+		const newData = await req.json();
+		return newData;
+	} catch (error) {
+		console.log("Fail To Save Data", error);
+	}
+};
+
+/* Function to update UI*/
+const updateUI = async () => {
+	const request = await fetch("/all");
+	try {
+		const allData = await request.json();
+		// update new entry values
+		document.getElementById("date").innerHTML = allData.date;
+		document.getElementById("temp").innerHTML = allData.temp;
+		document.getElementById("content").innerHTML = allData.content;
+	} catch (error) {
+		console.log(" Updated Failed", error);
+	}
+};
